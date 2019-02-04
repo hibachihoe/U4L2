21ccd0673ed1d5bdc6988830f78d188c86cb2210
hibachihoe <43859732+hibachihoe@users.noreply.github.com>
2019-02-04 11:24:50 -0500

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,5 @@
+
+classes=['english', 'math', 'history', 'science', 'gym', 'health', 'advisory', 'production']
+
+for subject in classes:
+	print('You have been enrolled in' +' '+ subject +'.')
---
+++ problem2.py
@@ -0,0 +1,4 @@
+numbers=['3', '5', '9', '17', 123, 666, 1000, 4321]
+for number in numbers:
+	print('When you double the number,' + str(number) + ' '+'you get:')
+	print(number * 2)
